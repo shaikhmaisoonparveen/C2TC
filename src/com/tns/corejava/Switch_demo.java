3b81defa79b16c5e9ee7466d35f12c8d4b11af5d
User <User@DESKTOP-78ILHSQ>
2022-04-16 11:09:09 -0700

    Ass

---
+++ src/com/tns/corejava/Switch_demo.java
@@ -0,0 +1,23 @@
+package com.tns.corejava;
+
+public class Switch_demo {
+	public static void main(String[] args) {
+		int x=1;
+		switch(x)
+		{
+		case 0:
+			System.out.println(0);
+			break;
+		case 1:
+		System.out.println(1);
+	//	break;
+		case 2:
+			System.out.println(2);
+			//break;
+			default:
+				System.out.println("default statement");
+
+	}
+
+	}
+}
